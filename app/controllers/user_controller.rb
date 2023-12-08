c1ba3a5f2de49db80b0c5e3fe59275d760917092
harriet-fisher <106377699+harriet-fisher@users.noreply.github.com>
2023-12-08 05:15:46 +0000

    done

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
   def show
-    @cover_letters = Message.where({ :user_id => current_user.id })
+    @cover_letters = Message.where({ :user_id => current_user.id }).order(created_at: :desc)
     render({ :template => "user/show" })
   end
 end
